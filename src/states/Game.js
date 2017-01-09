3fe3d5e31843a6d87a4754d60da2ddc2d91eb11d
Jon Kuperman <jon.kuperman@gmail.com>
2017-01-08 21:32:41 -0800

    added keyboard for movement

--- src/states/Game.js
+++ src/states/Game.js
@@ -53,11 +53,37 @@ export default class extends Phaser.State {
     //  Our two animations, walking left and right.
     this.player.animations.add('left', [0, 1, 2, 3], 10, true)
     this.player.animations.add('right', [5, 6, 7, 8], 10, true)
+
+    this.cursors = this.game.input.keyboard.createCursorKeys()
   }
 
   update () {
     //  Collide the player and the stars with the platforms
     this.game.physics.arcade.collide(this.player, this.platforms)
+    //  Reset the players velocity (movement)
+    this.player.body.velocity.x = 0
+
+    if (this.cursors.left.isDown) {
+      //  Move to the left
+      this.player.body.velocity.x = -150
+
+      this.player.animations.play('left')
+    } else if (this.cursors.right.isDown) {
+      //  Move to the right
+      this.player.body.velocity.x = 150
+
+      this.player.animations.play('right')
+    } else {
+      //  Stand still
+      this.player.animations.stop()
+
+      this.player.frame = 4
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.body.velocity.y = -350
+    }
   }
 
   render () {}
