ee4961d5d4976bcd294398166e04d4c4ae21b624
Jon Kuperman <jon.kuperman@gmail.com>
2017-01-08 21:28:04 -0800

    added person and gave him gravity

--- dist/bundle.js
+++ dist/bundle.js
@@ -9097,7 +9097,10 @@ webpackJsonp([0],[
 
 	  _createClass(_class, [{
 	    key: 'init',
-	    value: function init() {}
+	    value: function init() {
+	      this.player = null;
+	      this.platforms = null;
+	    }
 	  }, {
 	    key: 'preload',
 	    value: function preload() {}
@@ -9110,13 +9113,13 @@ webpackJsonp([0],[
 	      this.game.add.sprite(0, 0, 'sky');
 
 	      //  The platforms group contains the ground and the 2 ledges we can jump on
-	      var platforms = this.game.add.group();
+	      this.platforms = this.game.add.group();
 
 	      //  We will enable physics for any object that is created in this group
-	      platforms.enableBody = true;
+	      this.platforms.enableBody = true;
 
 	      // Here we create the ground.
-	      var ground = platforms.create(0, this.game.world.height - 64, 'ground');
+	      var ground = this.platforms.create(0, this.game.world.height - 64, 'ground');
 
 	      //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
 	      ground.scale.setTo(2, 2);
@@ -9125,21 +9128,38 @@ webpackJsonp([0],[
 	      ground.body.immovable = true;
 
 	      //  Now let's create two ledges
-	      var ledge = platforms.create(400, 400, 'ground');
+	      var ledge = this.platforms.create(400, 400, 'ground');
 
 	      ledge.body.immovable = true;
 
-	      ledge = platforms.create(-150, 250, 'ground');
+	      ledge = this.platforms.create(-150, 250, 'ground');
 
 	      ledge.body.immovable = true;
+
+	      // The player and its settings
+	      this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+
+	      //  We need to enable physics on the player
+	      this.game.physics.arcade.enable(this.player);
+
+	      //  Player physics properties. Give the little guy a slight bounce.
+	      this.player.body.bounce.y = 0.2;
+	      this.player.body.gravity.y = 300;
+	      this.player.body.collideWorldBounds = true;
+
+	      //  Our two animations, walking left and right.
+	      this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+	      this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+	    }
+	  }, {
+	    key: 'update',
+	    value: function update() {
+	      //  Collide the player and the stars with the platforms
+	      this.game.physics.arcade.collide(this.player, this.platforms);
 	    }
 	  }, {
 	    key: 'render',
-	    value: function render() {
-	      if (true) {
-	        this.game.debug.spriteInfo(this.mushroom, 32, 32);
-	      }
-	    }
+	    value: function render() {}
 	  }]);
 
 	  return _class;
